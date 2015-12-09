95f75cb9ac34f45246e495d41cebaea67cb9221e
Chaim Solomon <chaim@itcentralstation.com>
2015-12-09 20:48:15 +0200

    Updated to 2.2.0rc4

--- update.sh
+++ update.sh
@@ -21,8 +21,7 @@ mkdir $EXTRACT_DIRECTORY
 tar xfz $FILE_NAME -C $EXTRACT_DIRECTORY
 
 cp "$RELEASE_PATH/dist/parsley.js" $VENDOR_JS_PATH
-cp "$RELEASE_PATH/dist/parsley.remote.js" $VENDOR_JS_PATH
-cp "$RELEASE_PATH/src/parsley.css" $VENDOR_CSS_PATH
+cp "$RELEASE_PATH/dist/parsley.min.js" $VENDOR_JS_PATH
 
 for file in "$RELEASE_PATH/src/i18n/"*; do
   base_name=$(basename "$file")
